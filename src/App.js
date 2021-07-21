656a9e8904813fd97266198784568ca579065144
iago.bortolon <iago_bortolon@hotmail.com>
2021-07-21 10:01:02 -0300

    adicionando arquivos js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/listadeCategorias";
 import "./assets/App.css";
 import "./assets/index.css";
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       notas: [],
+      categorias: [],
     };
   }
   criarNota(titulo, texto) {
-    if (texto == "" && titulo == "") {
+    if (texto === "" && titulo === "") {
       return;
     }
     const novaNota = { titulo, texto };
@@ -21,11 +24,32 @@ class App extends Component {
     };
     this.setState(novoEstado);
   }
+
+  adicionarCategoria(nomeCategoria) {
+    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
+    const novoEstado = { ...this.state, categorias: novoArrayCategorias };
+    this.setState(novoEstado);
+  }
+  deletarNota(index) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+    this.setState({ notas: arrayNotas });
+  }
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas notas={this.state.notas} />
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            adicionarCategoria={this.adicionarCategoria.bind(this)}
+            categorias={this.state.categorias}
+          />
+          <ListaDeNotas
+            notas={this.state.notas}
+            apagarNota={this.deletarNota.bind(this)}
+            P
+          />
+        </main>
       </section>
     );
   }
---
+++ src/components/listadeCategorias/listaDeCategorias.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "./estilo.css";
+class ListaDeCategorias extends Component {
+  _handlerEventoInput(evento) {
+    if (evento.key === "Enter") {
+      let valorCategoria = evento.target.value;
+      this.props.adicionarCategoria(valorCategoria);
+    }
+  }
+  render() {
+    return (
+      <section className="lista-categorias">
+        <ul className="lista-categorias_lista">
+          {this.props.categorias.map((categoria, index) => {
+            return (
+              <li key={index} className="lista-categorias_item">
+                {categoria}
+              </li>
+            );
+          })}
+        </ul>
+        <input
+          type="text"
+          className="lista-categorias_input"
+          placeholder="Adicionar Categoria"
+          onKeyUp={this._handlerEventoInput.bind(this)}
+        />
+      </section>
+    );
+  }
+}
+
+export default ListaDeCategorias;
